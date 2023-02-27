555993ae69138669488baf18e0241967f41faa89
ModifiedBear <A01707550@tec.mx>
2023-02-27 10:45:55 -0600

    Added synth 2 and 3

---
+++ .ipynb_checkpoints/examples-checkpoint.py
@@ -0,0 +1,123 @@
+import numpy as np
+
+from qiskit import QuantumCircuit, Aer, assemble
+from numpy import pi
+
+from qplayer import extract_music, player
+from circuits import rotation_circuit, duration_gate, superpose_qutrit
+
+
+def phase_music(iters, basis):
+    """
+    Implements quantum algorithm where phases are added to each qubit based on the basis.
+    Iterates the algorithm to store various measures, and the plays them
+
+    Args:
+        iters (int): How many measures to make
+        basis (float): Fraction of rotationts
+    """
+    svsim = Aer.get_backend("aer_simulator")
+
+    measures = []
+
+    for i in range(iters):
+        phase = i * 2 * pi / basis
+        qc = QuantumCircuit(7)
+        rotations = rotation_circuit(
+            7, [phase / 1, phase / 1, phase / 1, phase / 1, phase / 1, phase / 1]
+        )
+        duration = duration_gate()
+
+        qc = qc.compose(rotations).compose(duration, 6)
+
+        qc.save_statevector()
+        qobj = assemble(qc)
+        result = svsim.run(qobj).result()
+        hist = result.get_counts()
+
+        notes, beats = extract_music(hist)
+
+        measures.append([notes, beats])
+
+    player(measures)
+
+
+def entanglement_music_1(iters):
+    svsim = Aer.get_backend("aer_simulator")
+
+    measures = []
+
+    for i in range(iters):
+        qc = QuantumCircuit(7)
+        duration = duration_gate()
+
+        qc = qc.compose(duration, 6)
+        time_circuit = superpose_qutrit(1, 0, 0)
+        note_circuit = superpose_qutrit(1, 0, 0)
+
+        qc = qc.compose(time_circuit, [0, 1, 2]).compose(note_circuit, [3, 4, 5])
+
+        qc.save_statevector()
+        qobj = assemble(qc)
+        result = svsim.run(qobj).result()
+        hist = result.get_counts()
+
+        notes, beats = extract_music(hist)
+
+        measures.append([notes, beats])
+
+    player(measures)
+
+
+def entanglement_music_2(iters):
+    svsim = Aer.get_backend("aer_simulator")
+
+    measures = []
+
+    for i in range(iters):
+        qc = QuantumCircuit(7)
+        duration = duration_gate()
+
+        qc = qc.compose(duration, 6)
+        time_circuit = superpose_qutrit(0, 1, 0)
+        note_circuit = superpose_qutrit(0, 1, 0)
+
+        qc = qc.compose(time_circuit, [0, 1, 2]).compose(note_circuit, [3, 4, 5])
+
+        qc.save_statevector()
+        qobj = assemble(qc)
+        result = svsim.run(qobj).result()
+        hist = result.get_counts()
+
+        notes, beats = extract_music(hist)
+
+        measures.append([notes, beats])
+
+    player(measures)
+
+
+def entanglement_music_3(iters):
+    svsim = Aer.get_backend("aer_simulator")
+
+    measures = []
+
+    for i in range(iters):
+        qc = QuantumCircuit(7)
+        duration = duration_gate()
+
+        qc = qc.compose(duration, 6)
+        time_circuit = superpose_qutrit(0, 0, 1)
+        note_circuit = superpose_qutrit(0, 0, 1)
+
+        qc = qc.compose(time_circuit, [0, 1, 2]).compose(note_circuit, [3, 4, 5])
+
+        qc.save_statevector()
+        qobj = assemble(qc)
+        result = svsim.run(qobj).result()
+        hist = result.get_counts()
+
+        notes, beats = extract_music(hist)
+
+        measures.append([notes, beats])
+
+    player(measures)
